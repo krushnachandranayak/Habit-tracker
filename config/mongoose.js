edd7150d6492b36f84fe724c6a87890bea0cac8e
KrushnaChandra Nayak <131634927+krushnachandranayak@users.noreply.github.com>
2023-08-14 15:56:18 +0530

    update commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // storing the db on mongo atlas
-const DB = "mongodb+srv://krushnachandranayak308:Sunil123@cluster0.qh9m6a4.mongodb.net/";
+const DB = 'mongodb+srv://krushnachandranayak308:Sunil123@cluster0.qh9m6a4.mongodb.net/';
 
 mongoose.connect(DB).then(()=>{
     console.log('Connection successful!');
